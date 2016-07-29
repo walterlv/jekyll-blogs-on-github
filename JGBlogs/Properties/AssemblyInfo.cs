11b9e0fcda70f7e938a0e4c101e69ca0e818aec9
walterlv <xblero@gmail.com>
2016-07-29 23:18:58 +0800

    添加程序清单

--- JGBlogs/Properties/AssemblyInfo.cs
+++ JGBlogs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JGBlogs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("view jekyll blogs on github.io")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("walterlv@outlook.com")]
 [assembly: AssemblyProduct("JGBlogs")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © walterlv 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
